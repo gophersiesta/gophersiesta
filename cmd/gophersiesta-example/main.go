5b82846af7f86540b1c921ec0ee3f15031c89b0f
Daniel Esteban <conejo@conejo.me>
2016-02-07 12:06:21 +0100

    fix example

--- cmd/gophersiesta-example/main.go
+++ cmd/gophersiesta-example/main.go
@@ -22,8 +22,8 @@ import (
 
 func main() {
 
-	//appname := "app1"
-	//labels := []string{""}
+	appname := "app1"
+	labels := []string{""}
 
 	api := common.NewAPI("http://localhost:4747")
 	api.Debug(true)
@@ -31,30 +31,29 @@ func main() {
 	fmt.Println("GET APPS ===")
 	fmt.Println(api.GetApps())
 
-	/*
-		fmt.Println("GET TEMPLATE ===")
-		fmt.Println(api.GetTemplate(appname))
+	fmt.Println("GET TEMPLATE ===")
+	fmt.Println(api.GetTemplate(appname))
 
-		fmt.Println("GET PLACEHOLDERS ===")
-		fmt.Println(api.GetPlaceholders(appname))
+	fmt.Println("GET PLACEHOLDERS ===")
+	fmt.Println(api.GetPlaceholders(appname))
 
-		fmt.Println("GET LABELS ===")
-		fmt.Println(api.GetLabels(appname))
+	fmt.Println("GET LABELS ===")
+	fmt.Println(api.GetLabels(appname))
 
-		fmt.Println("GET VALUES ===")
-		fmt.Println(api.GetValues(appname, labels))
+	fmt.Println("GET VALUES ===")
+	fmt.Println(api.GetValues(appname, labels))
 
-		fmt.Println("SET VALUES ===")
-		v1 := common.Value{"name1", "value1"}
-		v2 := common.Value{"name2", "value2"}
-		v3 := common.Value{"name3", "value3"}
-		values := common.Values{[]*common.Value{&v1, &v2, &v3}}
-		fmt.Println(api.SetValues(appname, labels, values))
+	fmt.Println("SET VALUES ===")
+	v1 := common.Value{"name1", "value1"}
+	v2 := common.Value{"name2", "value2"}
+	v3 := common.Value{"name3", "value3"}
+	values := common.Values{[]*common.Value{&v1, &v2, &v3}}
+	fmt.Println(api.SetValues(appname, labels, values))
 
-		fmt.Println("GET VALUES (AGAIN) ===")
-		fmt.Println(api.GetValues(appname, labels))
+	fmt.Println("GET VALUES (AGAIN) ===")
+	fmt.Println(api.GetValues(appname, labels))
 
-		fmt.Println("RENDER ===")
-		fmt.Println(api.Render(appname, labels, "json"))
-	*/
+	fmt.Println("RENDER ===")
+	fmt.Println(api.Render(appname, labels, "json"))
+
 }
